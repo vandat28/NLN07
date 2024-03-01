b06ff671f998c546bf04559f5333eb4775c38c27
vandat28 <vandat2801@gmail.com>
2024-03-01 15:23:31 +0700

    dat commit 3/1

--- frontend/src/components/admin/AdminProduct.js
+++ frontend/src/components/admin/AdminProduct.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
 
 
+
 export default function AdminProduct() {
 
 
@@ -24,44 +25,130 @@ export default function AdminProduct() {
                     <option value="Type B">Type B</option>
                     {/* Add more options for product types */}
                 </select>
-                <select value="Trạng thái" >
-                    <option value="">- Chọn trạng thái -</option>
+                <select>
+                    <option>- Chọn trạng thái -</option>
                     <option value="Active">Active</option>
                     <option value="Inactive">Inactive</option>
                     {/* Add more options for product statuses */}
                 </select>
                 <div className="filter">
                     <button className="filter-button">Lọc</button>
+                    <button className="filter-button" data-bs-toggle="modal" data-bs-target="#staticBackdrop2">Xem danh mục</button>
+                    <button className="filter-button" data-bs-toggle="modal" data-bs-target="#staticBackdrop">Thêm sản phẩm</button>
                 </div>
             </div>
             <div className="product-table">
                 <table>
                     <thead>
                         <tr>
-                            <th>Sản phẩm</th>
+                            <th className='color-blue'>Sản phẩm</th>
                             <th>Loại</th>
                             <th>Trạng thái</th>
-                            <th>Giá bán</th>
+                            <th className='color-blue'>Giá bán</th>
                             <th>Tùy chọn</th>
                         </tr>
                     </thead>
                     <tbody>
                         {products.map(product => (
                             <tr key={product.id}>
-                                <td>{product.id}</td>
+                                <td className='color-blue'>{product.id}</td>
                                 <td>{product.name}</td>
                                 <td>{product.type}</td>
-                                <td>{product.status}</td>
+                                <td className='color-blue'>{product.status}</td>
+                                <td>
+                                    <button type="button"><i class="fa-solid fa-trash"></i></button>
+                                    <button type="button"><i class="fa-solid fa-pen-to-square"></i></button>
+                                </td>
                             </tr>
                         ))}
                     </tbody>
                 </table>
             </div>
-            <div class="pagination">
+            <div className="pagination">
                 <p>Tổng số trang: 102 </p>
-                <a href="#" class="prev">&laquo;</a>
-                <input class="page" value={1} />
-                <a href="#" class="next"> &raquo;</a>
+                <a href="#" className="prev">&laquo;</a>
+                <input className="page" value={1} />
+                <a href="#" className="next"> &raquo;</a>
+            </div>
+            <form className="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabIndex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+                <div className="modal-dialog modal-dialog-centered">
+                    <div className="modal-content">
+                        <div className="modal-header">
+                            <h5 className="modal-title">Thêm sản phẩm</h5>
+                        </div>
+                        <div className="modal-body">
+                            <div className='input-grid'>
+                                <div className="input-container">
+                                    <label className="input-label">Tên sản phẩm</label>
+                                    <input type="text" className="input-field" name="name" />
+                                </div>
+                                <div className="input-container">
+                                    <label className="input-label">Hình ảnh</label>
+                                    <input type="text" className="input-field" name="image" />
+                                </div>
+                                <div className="input-container">
+                                    <label className="input-label">Giá bán</label>
+                                    <input type="text" className="input-field" name="price" />
+                                </div>
+                                <div className="input-container">
+                                    <label className="input-label">Mô tả</label>
+                                    <input type="text" className="input-field" name="description" />
+                                </div>
+                                <div className="input-container">
+                                    <label className="input-label">Loại sản phẩm</label>
+                                    <input type="text" className="input-field" name="description" />
+                                </div>
+                                <div className="input-container">
+                                    <label className="input-label">Số lượng</label>
+                                    <input type="text" className="input-field" name="description" />
+                                </div>
+                            </div>
+
+                        </div>
+                        <div className="modal-buttons">
+                            <button type="button" className="modal-button close-button" data-bs-dismiss="modal">Đóng</button>
+                            <button type="submit" className="modal-button save-button">Lưu</button>
+                        </div>
+                    </div>
+                </div>
+            </form>
+            <div className="modal fade" id="staticBackdrop2" data-bs-backdrop="static" data-bs-keyboard="false" tabIndex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+                <div className="modal-dialog modal-dialog-centered">
+                    <div className="modal-content">
+                        <div className="modal-header">
+                            <h5 className="modal-title">Danh mục sản phẩm</h5>
+                        </div>
+                        <div className="add-category">
+                            <input type="text" className="category-input" placeholder="Nhập tên danh mục" />
+                            <button type="button" className="add-button">Thêm danh mục</button>
+                        </div>
+
+                        <div className="modal-body">
+                            <table className="category-table">
+                                <thead>
+                                    <tr>
+                                        <th>ID</th>
+                                        <th>Tên danh mục</th>
+                                        <th>Tùy chọn</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <tr>
+                                        <td>1</td>
+                                        <td>Danh mục 1</td>
+                                        <td>
+                                            <button type="button"><i class="fa-solid fa-trash"></i></button>
+                                            <button type="button"><i class="fa-regular fa-pen-to-square"></i></button>
+                                        </td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                        </div>
+                        <div className="modal-buttons">
+                            <button type="button" className="modal-button close-button" data-bs-dismiss="modal">Đóng</button>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     )
